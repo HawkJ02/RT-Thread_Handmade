53997161afd32eb3d41c879e05e1876f5a2f7d2d
HawkJ02 <1507992088@qq.com>
2023-12-07 10:29:54 +0800

    first commit

---
+++ My_RTThread/Rt-Thread/Inc/rtconfig.h
@@ -0,0 +1,10 @@
+#ifndef	__RTTHREAD_CFG_H__
+#define __RTTHREAD_CFG_H__
+
+/* 字节对齐 */
+#define RT_ALIGN_SIZE										4
+
+/* 最大线程优先级 */
+#define RT_THREAD_PRIORITY_MAX					32
+
+#endif
---
+++ README.md
@@ -0,0 +1,1 @@
+# RT-Thread_Handmade
